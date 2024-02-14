61e25a3a7a7091a422c0bbb373126f320c3f81bc
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2024-02-15 00:17:30 +0900

    20240215 jwt auth example

--- main.go
+++ main.go
@@ -1,30 +1,101 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(http.StatusOK, "Hello, World!")
 	})
 
-	router.GET("/secret", basicAuthMiddleware, func(c *gin.Context) {
-		c.String(200, "success!")
-	})
+	router.POST("/log-in", LogInHandler)
+	router.GET("/verify", VerifyHandler)
 
 	router.Run(":8080")
 }
 
-func basicAuthMiddleware(c *gin.Context) {
-	username, password, ok := c.Request.BasicAuth()
+type LogInRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-	if !ok || username != "semyeong" || password != "000130" {
-		c.AbortWithStatus(401)
+func LogInHandler(c *gin.Context) {
+	var req *LogInRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid request")
 		return
 	}
 
-	c.Next()
+	if req.Username != "semyeong" || req.Password != "000130" {
+		c.String(http.StatusUnauthorized, "Incorrect username or password")
+		return
+	}
+
+	token, err := CreateToken(req.Username)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "CreateToken error")
+		return
+	}
+
+	c.String(http.StatusOK, token)
+}
+
+func VerifyHandler(c *gin.Context) {
+	tokenString := c.GetHeader("Authorization")
+	if tokenString == "" {
+		c.String(http.StatusUnauthorized, "Missing authorization header")
+		return
+	}
+
+	tokenString = tokenString[len("Bearer "):]
+
+	err := VerifyToken(tokenString)
+	if err != nil {
+		c.String(http.StatusUnauthorized, "Invalid Token")
+		return
+	}
+
+	c.String(http.StatusOK, "Valid Token!")
+}
+
+var secretKey = []byte("secret-key")
+
+func CreateToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
 }
